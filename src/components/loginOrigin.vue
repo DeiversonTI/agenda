42cc6665981dcb1dc3cd194b487c1a361f3be1f8
DeiversonTI <deiverson.dtg@gmail.com>
2022-07-21 17:22:20 -0300

    update página principal

--- src/components/loginOrigin.vue
+++ src/components/loginOrigin.vue
@@ -1,57 +1,49 @@
 <template>
-    <div class="  flex justify-center items-center   ">
+    <div class="bg-gray-100 w-screen h-screen flex justify-center items-center  ">
        
-            <div class="flex gap-4 items-center justify-center  w-full">
-            <!-- <div class="lg:px-40 xl:px-56 lg:h-80 md:h-80 sm:h-80 xl:h-80 xl:w-3/4 2xl:h-80  2xl:w-3/5 sm:px-11 w-full mx-4 sm:flex sm:justify-center sm:items-center bg-gray-300  "> -->
-                <div class="hidden md:visible md:flex justify-center items-center  w-full">
-                    <div>
-                       <img  src="../assets/home-logo.jpg" alt="" style="max-width:100% ;">
-                    </div>
-                </div>
-                   
-                <!-- <div class=" sm:py-12 sm:rounded-none sm:rounded-l-lg flex items-center justify-center w-full bg-blue-300 sm:w-2/4 h-full rounded-t-lg  ">
-                <div class=" justify-center items-center py-1 ">
-                    <img src="../assets/Agendamentos.png" alt="" class="w-44 2xl:w-full md:w-full lg:w-full sm:w-full xl:w-full ">
-                    
+            <div class="lg:px-40 xl:px-56 lg:h-80 md:h-80 sm:h-80 xl:h-80 xl:w-3/4 2xl:h-80  2xl:w-3/5 sm:px-11 w-full mx-4 sm:flex sm:justify-center sm:items-center  ">
+                <div>
+                  
+                    <!-- <div>
+                        <h1 class="text-center text-white text-2xl">Bem Vindo ao PlaAg</h1>
+                    </div> -->
                 </div>
 
-            </div>
-            -->
-                <div class=" w-full h-screen  bg-gradient-to-l from-blue-100   ">
+                <div class=" sm:py-12 sm:rounded-none sm:rounded-l-lg flex items-center justify-center w-full bg-blue-300 sm:w-2/4 h-full rounded-t-lg  ">
+                    <div class=" justify-center items-center py-1 ">
+                        <img src="../assets/Agendamentos.png" alt="" class="w-44 2xl:w-full md:w-full lg:w-full sm:w-full xl:w-full ">
+                        
+                    </div>
+
+                </div>
+             
+                <div class="sm:w-full bg-gray-50 h-full sm:h-80 sm:rounded-r-lg sm:rounded-bl-none rounded-b-lg ">
                     
-                        <div class="h-2/6 mx-auto items-center flex justify-center ">
-                            <img class="w-52 md:w-2/5" src="../assets/Agendamentos.png" alt="" >
-                        </div>
-                        <div class=" bg-white px-8 py-4 mx-1 md:mx-8 rounded-lg shadow-xl border border-gray-100">
-                        <form>
-                            <h2 class="py-1 text-center text-6x1  text-gray-700 sm:mb-4 sm:mt-4 md:text-3xl md:mb-1 md:mt-4">LOGIN</h2>
+                        <form  class="bg-white rounded-lg sm:h-80 px-3 pt-2 pb-6 mb-2 w-full  shadow-2xl ">
+                            <h2 class="py-1 text-center text-3xl font-thin text-gray-700 sm:mb-2 2xl:text-xl 2xl:mb-1">LOGIN</h2>
                             <div class="mb-4">
-                                <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                                    Email
-                                </label>
-                                <input v-model="email"  required class="pl-8 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Entre com o Email">
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                               Email
+                            </label>
+                            <input v-model="email"  required class="pl-8 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Entre com o Email">
                             </div>
 
                             <div class="mb-6 sm:mb-2">
-                                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                                    Senha
-                                </label>
-                                <input v-model="password" required class="pl-8 hadow appearance-none border focus:border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Digite sua Senha">
-                                <!-- <p class="text-red-500 text-xs italic">Please choose a password.</p> -->
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                                Senha
+                            </label>
+                            <input v-model="password" required class="pl-8 hadow appearance-none border focus:border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Digite sua Senha">
+                            <!-- <p class="text-red-500 text-xs italic">Please choose a password.</p> -->
                             </div>
-                            <div class="w-full text-center m-auto  md:flex md:items-center md:justify-between sm:pt-1 ">
-                                <div>
-                                    <button @click.prevent="login()"  class="bg-gradient-to-r from-blue-500 to-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-16 rounded focus:outline-none focus:shadow-outline" >
-                                        Logar
-                                    </button>
-                                </div>
-                                <div>
-                                    <router-link to="resetPassword">
-                                        <p class=" cursor-pointer inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
-                                            Esqueceu a senha?
-                                        </p>
-                                    </router-link>
-                                </div>
+                            <div class="flex items-center justify-between sm:pt-1">
+                            <button @click.prevent="login()"  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" >
+                                Logar
+                            </button>
+                            <router-link to="resetPassword">
+                                <p class=" cursor-pointer inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
+                                    Esqueceu a senha?
+                                </p>
+                            </router-link>
                             </div>
                             <div  class="justify-between text-gray-900 items-center flex text-center w-full  sm:mt-3 mt-8 2xl:mt-5">
                                     <div class="flex items-center ">
@@ -68,8 +60,7 @@
                             </div>
                             
                         </form>
-                        </div>
-                
+                   
                 </div>
                
             </div>
